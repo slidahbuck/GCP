af87eea357586d5e16e41c97f00b8fda1ac9db9b
slidahbuck <slidah60@gmail.com>
2025-12-05 10:09:32 -1000

    yehh

--- dolphins.py
+++ dolphins.py
@@ -55,7 +55,7 @@ model.compile(
 )
 
 # train the model
-history = model.fit(X_train, y_train, epochs=500, validation_split=.4774)
+history = model.fit(X_train, y_train, epochs=500, validation_data=(X_test, y_test))
 
 # plot graph 
 df = pd.DataFrame(history.history)
